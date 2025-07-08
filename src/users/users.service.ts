1e6282f5703eacd5060d89ac15edffea771f5495
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-07-09 04:07:41 +0600

    add: user details get update and delete

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,4 +7,25 @@ export class UsersService {
   async getUsers() {
     return await this.prisma.user.findMany();
   }
+  async getUserById(id: number) {
+    return await this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+  async createUser(data: { name: string; email: string }) {
+    return await this.prisma.user.create({
+      data,
+    });
+  }
+  async updateUser(id: number, data: { name?: string; email?: string }) {
+    return await this.prisma.user.update({
+      where: { id },
+      data,
+    });
+  }
+  async deleteUser(id: number) {
+    return await this.prisma.user.delete({
+      where: { id },
+    });
+  }
 }
